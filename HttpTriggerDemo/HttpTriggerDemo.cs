d4ef2be457f702fb362ede20aea847ce52cce0be
Prabin Paudel <paudelp@gmail.com>
2021-06-30 18:04:24 -0400

    Adding a demo Azure Functions

---
+++ HttpTriggerDemo/HttpTriggerDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using HashidsNet;
+
+namespace HttpTriggerDemo
+{
+    public static class HttpTriggerDemo
+    {
+        [FunctionName("HttpTriggerDemo")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var hashIds = new Hashids("My Salt", 6, "abcdefghijklmnopqrstuvwxyz0123456789");
+            var name = hashIds.Encode(1);
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
